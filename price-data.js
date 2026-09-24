fbf0894022b9fe5de05f8946b5484fbf19b34bcb
rgt77 <34492942+rgt77@users.noreply.github.com>
2026-09-24 09:27:53 +0200

    refactor: rename national price dataset

---
+++ price-data.js
@@ -0,0 +1,13 @@
+window.COUNTY_PRICES = {
+  updatedAt: "2026-09-24",
+  retrievedAt: "2026-09-24",
+  source: "https://www.carculated.se/bensinpriser",
+  national: {
+    id: "riket",
+    name: "Hela Sverige",
+    petrol: 17.23,
+    petrol98: 17.98,
+    e85: 14.89,
+    diesel: 22.14
+  }
+};
